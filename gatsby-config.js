3220c013eb94a122934954813c5f462be780422e
jromest <jromestiller@gmail.com>
2019-06-04 00:01:26 +0800

    update metadata for reactjs ph

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `ReactJS Philippines`,
+    description: `ReactJS Enthusiasts in the Philippines`,
+    author: `ReactJS PH`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `ReactJS PHilippines`,
+        short_name: `ReactJS PH`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
