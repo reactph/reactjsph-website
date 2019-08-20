fa4bb6bc9affd392d3fd0f8c09d5d7bbd4545c94
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2019-08-20 17:46:44 +0800

    🔧 Update packages. Configure Rebass/Emotion.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,14 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from "react"
+import { ThemeProvider } from "emotion-theming"
+import theme from "@rebass/preset"
+
+import "./src/reset.css"
+
+// eslint-disable-next-line import/prefer-default-export
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={theme}>{element}</ThemeProvider>
+)
