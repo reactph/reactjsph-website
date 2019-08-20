56e967a58ff15513af942fe14fb498789ceb6423
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2019-08-20 17:46:44 +0800

    🔧 Update packages. Configure Rebass/Emotion.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
+import { css } from "@emotion/core"
+import { Box } from "rebass"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+// Sample styling pattern
+const styles = {
+  imageWrapper: css`
+    max-width: 300px;
+    margin-bottom: 1.45rem;
+  `,
+}
 
 const IndexPage = () => (
   <Layout>
@@ -11,9 +21,19 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+
+    {/* Sample implementation of Rebass and emotion */}
+    <Box
+      py={90}
+      css={css`
+        background: red;
+      `}
+    >
+      <div css={styles.imageWrapper}>
+        <Image />
+      </div>
+    </Box>
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
