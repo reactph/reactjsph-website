fce0e2ff0fe18573928d73493204a4b97b9f8b40
jromest <jromestiller@gmail.com>
2019-08-30 17:40:20 +0800

    update manifest data and colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,11 +25,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `ReactJS PHilippines`,
+        name: `ReactJS Philippines`,
         short_name: `ReactJS PH`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#0c1d60`,
+        theme_color: `#0c1d60`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
