1ed386ee8e74ba7c2c5285b307e8549182d12a78
Ericson Luciano <design.ericson@gmail.com>
2019-10-02 22:32:39 +0800

    Add Header and Menu
    Create a header with menu
    - Added a simple transition on menu on hover.
    - Added logo on images assets
    
    Still need to do responsive. and removed the background for hero purposes.

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,112 @@
-import { Link } from "gatsby"
+import { Link as GatsbyLink } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import { Box, Text } from "rebass"
+import { Box, Image, Flex, Link } from "rebass"
+import Logo from "../images/logo.svg"
+import Container from "./container"
 
 const Header = ({ siteTitle }) => (
-  <Box as="header" mb={2} bg="darkBlue">
-    <Box m="0 auto" maxWidth={960} px={1} py={2}>
-      <Text as="h1" fontSize={5} variant="heading">
-        <Link
+  <Box mb={0} as="header">
+    <Container>
+      <Flex color="white" alignItems="center" py={0}>
+        <GatsbyLink
           to="/"
           style={{
             color: `white`,
             textDecoration: `none`,
           }}
-        >
-          {siteTitle}
-        </Link>
-      </Text>
-    </Box>
+        />
+        <Image
+          src={Logo}
+          alt={siteTitle}
+          sx={{
+            width: ["206px", "50%"],
+          }}
+        />
+        <Box mx="auto" />
+        <Box>
+          <Link
+            mx={2}
+            p={0}
+            variant="nav"
+            href="/meetups"
+            sx={{
+              display: "inline-block",
+              transition: "400ms ease all",
+              textTransform: "uppercase",
+              "&:hover": {
+                color: "gold",
+              },
+              "&::after": {
+                content: '""',
+                display: "block",
+                width: "0",
+                height: "2px",
+                background: "gold",
+                transition: "width 300ms",
+              },
+              "&:hover::after": {
+                width: "100%",
+              },
+            }}
+          >
+            Meetups
+          </Link>
+          <Link
+            mx={2}
+            p={0}
+            variant="nav"
+            href="/blog"
+            sx={{
+              transition: "400ms ease all",
+              textTransform: "uppercase",
+              "&:hover": {
+                color: "gold",
+              },
+              "&::after": {
+                content: '""',
+                display: "block",
+                width: "0",
+                height: "2px",
+                background: "gold",
+                transition: "width 300ms",
+              },
+              "&:hover::after": {
+                width: "100%",
+              },
+            }}
+          >
+            Blog
+          </Link>
+          <Link
+            mx={2}
+            p={0}
+            variant="nav"
+            href="/jobs"
+            sx={{
+              transition: "400ms ease all",
+              textTransform: "uppercase",
+              "&:hover": {
+                color: "gold",
+              },
+              "&::after": {
+                content: '""',
+                display: "block",
+                width: "0",
+                height: "2px",
+                background: "gold",
+                transition: "width 300ms",
+              },
+              "&:hover::after": {
+                width: "100%",
+              },
+            }}
+          >
+            Jobs
+          </Link>
+        </Box>
+      </Flex>
+    </Container>
   </Box>
 )
 
