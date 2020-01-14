87aea0fbf1862c649939a7e8bceb545caa8c23b0
jromest <jromestiller@gmail.com>
2020-01-14 22:27:21 +0800

    :sparkles: create meetup section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,17 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/hero"
 import MissionSection from "../components/MissionSection"
+import MeetupSection from "../components/meetupSection"
 import VisionSection from "../components/VisionSection"
 import TeamSection from "../components/team"
 import Contact from "../components/contact"
-
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
     <MissionSection />
+    <MeetupSection />
     <VisionSection />
     <TeamSection />
     <Contact />
