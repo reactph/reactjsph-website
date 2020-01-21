eed9266cde14d82ee60cac50b6c652ee574a00f9
jromest <jromestiller@gmail.com>
2020-01-21 22:52:30 +0800

    :recycle: uniform component declaration to arrow functions for consistency

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,58 +4,56 @@ import { Box, Text, Button } from "rebass"
 import Container from "./container"
 import { generate3dShadow } from "../utils"
 
-const Contact = () => {
-  return (
-    <Box backgroundColor="darkBlue" py={2}>
-      <Container>
-        <Text
-          fontSize={6}
-          fontWeight="bold"
-          color="lightBlue"
-          mb={2}
-          maxWidth="51rem"
-          width="100%"
-          sx={{
-            textTransform: "uppercase",
-          }}
-        >
-          Sponsor A meet-up{" "}
-          <Text as="span" color="gold">
-            or just get in touch.
-          </Text>
+const Contact = () => (
+  <Box backgroundColor="darkBlue" py={2}>
+    <Container>
+      <Text
+        fontSize={6}
+        fontWeight="bold"
+        color="lightBlue"
+        mb={2}
+        maxWidth="51rem"
+        width="100%"
+        sx={{
+          textTransform: "uppercase",
+        }}
+      >
+        Sponsor A meet-up{" "}
+        <Text as="span" color="gold">
+          or just get in touch.
         </Text>
+      </Text>
 
-        <form>
-          <Box
-            sx={{
-              display: "grid",
-              gridTemplateColumns: "repeat(3, 1fr)",
-              gridTemplateRows: "auto 1fr",
-              gridColumnGap: "2rem",
-              gridRowGap: "2rem",
-            }}
-            mb={1}
-          >
-            <Box sx={{ gridArea: "1 / 1 / 2 / 2" }}>
-              <FormInput name="Name" label="Name" type="text" />
-            </Box>
-            <Box sx={{ gridArea: "2 / 1 / 3 / 2" }}>
-              <FormInput name="Email" label="Email" type="text" />
-            </Box>
-            <Box sx={{ gridArea: "1 / 2 / 3 / 4" }}>
-              <FormInput name="Message" label="Message" type="textarea" />
-            </Box>
+      <form>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "repeat(3, 1fr)",
+            gridTemplateRows: "auto 1fr",
+            gridColumnGap: "2rem",
+            gridRowGap: "2rem",
+          }}
+          mb={1}
+        >
+          <Box sx={{ gridArea: "1 / 1 / 2 / 2" }}>
+            <FormInput name="Name" label="Name" type="text" />
           </Box>
-          <Box textAlign="right">
-            <Button variant="primary" type="submit">
-              Send »
-            </Button>
+          <Box sx={{ gridArea: "2 / 1 / 3 / 2" }}>
+            <FormInput name="Email" label="Email" type="text" />
           </Box>
-        </form>
-      </Container>
-    </Box>
-  )
-}
+          <Box sx={{ gridArea: "1 / 2 / 3 / 4" }}>
+            <FormInput name="Message" label="Message" type="textarea" />
+          </Box>
+        </Box>
+        <Box textAlign="right">
+          <Button variant="primary" type="submit">
+            Send »
+          </Button>
+        </Box>
+      </form>
+    </Container>
+  </Box>
+)
 
 const FormInput = ({ name, label, type }) => (
   <Box
