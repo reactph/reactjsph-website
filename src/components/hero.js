9612e5c11662f9fc9f39adf2883151c63517fc32
jromest <jromestiller@gmail.com>
2020-01-29 08:37:54 +0800

    :lipstick: add responsive styles in spacing and font in hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -12,7 +12,7 @@ const Hero = () => (
       backgroundPosition: "center",
       backgroundSize: "cover",
       width: "100%",
-      height: "120vh",
+      height: ["100vh", "120vh"],
     }}
   >
     <Box
@@ -31,7 +31,7 @@ const Hero = () => (
       alignItems="center"
       css={{
         position: "absolute",
-        top: "-5vh",
+        top: [0, "-5vh"],
         left: 0,
         width: "100%",
         height: "100%",
@@ -40,21 +40,41 @@ const Hero = () => (
       }}
     >
       <Container>
-        <Text fontSize={5} color="gold" fontWeight="body" zIndex={2}>
-          ★★★ ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
+        <Text
+          fontSize={[4, 5]}
+          color="gold"
+          fontWeight="body"
+          zIndex={2}
+          mb={1}
+          sx={{
+            position: "relative",
+            ":after": {
+              content: "''",
+              position: "absolute",
+              left: ["150px", "200px"],
+              top: 0,
+              bottom: 0,
+              margin: "auto 0",
+              width: "40%",
+              height: "2px",
+              backgroundColor: "gold",
+            },
+          }}
+        >
+          ★ ★ ★
         </Text>
-        <Text fontSize={6} fontWeight="hero" color="white" zIndex={2}>
+        <Text fontSize={[5, 6]} fontWeight="hero" color="white" zIndex={2}>
           WE ARE
         </Text>
-        <Text fontSize={5} fontWeight="hero" color="lightBlue" zIndex={2}>
+        <Text fontSize={[4, 5]} fontWeight="hero" color="lightBlue" zIndex={2}>
           REACTJS
         </Text>
-        <Text fontSize={5} fontWeight="hero" color="gold" zIndex={2}>
+        <Text fontSize={[4, 5]} fontWeight="hero" color="gold" zIndex={2}>
           PHILIPPINES
         </Text>
         <Button
           sx={{
-            marginTop: 3,
+            marginTop: [2, 3],
             py: 2,
             px: 3,
             outline: 0,
