beaffa0b993266c1da57d73b22284af7d6e47561
jromest <jromestiller@gmail.com>
2020-02-04 21:28:04 +0800

    :sparkles: create useOnClickOutside hook

--- src/utils.js
+++ src/utils.js
@@ -35,3 +35,21 @@ export const useWindowSize = () => {
 
   return windowSize
 }
+
+export const useOnClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = e => {
+      if (!ref.current || ref.current.contains(e.target)) {
+        return
+      }
+
+      handler(e)
+    }
+
+    document.addEventListener("click", listener)
+
+    return () => {
+      document.removeEventListener("click", listener)
+    }
+  }, [ref, handler])
+}
