09e673b0ae624d8eea421f014c6352b747ad4221
galacemiguel <galacemiguel@gmail.com>
2020-03-24 22:10:51 +0800

    Add border to team images

--- src/components/teamMember.js
+++ src/components/teamMember.js
@@ -25,11 +25,15 @@ const TeamMember = ({ member: { photo, name, role }, ...props }) => (
       mb={[0, null, 1]}
       mr={[2, null, 0]}
       sx={{
+        boxSizing: "content-box",
         objectFit: "cover",
         objectPosition: "center",
         minWidth: ["5em", null, "174px"],
         maxWidth: 0,
         borderRadius: "50%",
+        borderWidth: ["3px", null, "4px"],
+        borderStyle: "solid",
+        borderColor: "white",
         transition: "200ms transform",
         boxShadow: "main",
       }}
