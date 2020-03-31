3c3de0a0b4396a3edaaad50dc1fe08bced0dd583
galacemiguel <galacemiguel@gmail.com>
2020-03-31 19:48:55 +0800

    Add gatsby-plugin-react-svg

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,5 +61,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /\.inline\.svg$/,
+        },
+      },
+    },
   ],
 }
