2211648e526d64549cfacd630dda87cb782a5591
jromest <jromestiller@gmail.com>
2020-04-24 21:15:12 +0800

    :sparkles: implement upcoming and past event in meetup section

--- src/components/meetupSection.js
+++ src/components/meetupSection.js
@@ -1,13 +1,11 @@
-/* eslint-disable camelcase */
 import React from "react"
-import { Box, Button, Flex, Text } from "rebass"
-import { format } from "date-fns"
+import { Box, Button, Text } from "rebass"
+import { isFuture, isPast } from "date-fns"
 import { useStaticQuery, graphql } from "gatsby"
 
 import theme from "../theme"
 import Container from "./container"
-
-const formatDate = date => format(new Date(date.split("T")[0]), "dd MMM yyyy")
+import MeetupEventCard from "./meetupEventCard"
 
 const facebookEventQuery = graphql`
   query FacebookEventQuery {
@@ -35,6 +33,11 @@ const MeetupSection = () => {
       events: { data },
     },
   } = useStaticQuery(facebookEventQuery)
+
+  const upcomingEvents = data.filter(event =>
+    isFuture(new Date(event.start_time))
+  )
+  const pastEvents = data.filter(event => isPast(new Date(event.start_time)))
 
   return (
     <Box pb={[7, 9]}>
@@ -64,74 +67,51 @@ const MeetupSection = () => {
           Community Meet‑ups
         </Text>
 
-        {data.map(({ id, name, start_time, place }) => {
-          const city = place?.location?.city
-
-          return (
-            <Flex
-              key={id}
-              as="li"
-              bg="white"
-              p={2}
+        {upcomingEvents.length >= 1 && (
+          <>
+            <Text
+              as="h3"
+              fontSize={[2, 3]}
+              color="white"
+              fontWeight="heading"
               mb={2}
-              color="darkBlue"
-              alignItems={["start", null, "center"]}
-              flexDirection={["column", null, "row"]}
-              sx={{
-                cursor: "pointer",
-                transition: "transform 200ms",
-                ":hover": {
-                  transform: "translateY(-0.25rem)",
-                },
-              }}
+              sx={{ textTransform: "uppercase" }}
             >
-              <Box
-                as="time"
-                dateTime={start_time}
-                pr={2}
-                mr={2}
-                fontSize={[1, null, 2]}
-                sx={{
-                  textTransform: "uppercase",
-                  borderRightWidth: "1px",
-                  borderRightStyle: "solid",
-                  borderRightColor: ["transparent", null, "darkBlue"],
-                }}
-              >
-                {formatDate(start_time)}
-              </Box>
+              Upcoming
+            </Text>
 
-              <Box
-                flex={1}
-                pt={[1, null, 0]}
-                pb={[2, null, 0]}
-                mr={[0, null, 2]}
-              >
-                <Text
-                  as="h4"
-                  fontSize={[2, null, 3]}
-                  fontWeight="heading"
-                  sx={{ textTransform: "uppercase" }}
-                >
-                  {name}
-                </Text>
-                <Text>{`${place.name}${city ? `, ${city}` : ""}`}</Text>
-              </Box>
+            <Box as="ul">
+              {upcomingEvents.map(event => (
+                <MeetupEventCard key={event.id} event={event} />
+              ))}
+            </Box>
 
-              <Button
-                as="a"
-                href={`https://facebook.com/events/${id}/`}
-                target="_blank"
-                rel="noopener noreferrer"
-                variant="outline"
-                mr="6px"
-                mb="6px"
-              >
-                ↗ Open
-              </Button>
-            </Flex>
-          )
-        })}
+            <Box
+              as="hr"
+              my={3}
+              mx="auto"
+              sx={{ borderColor: "white", height: "2px", maxWidth: "170px" }}
+            />
+
+            <Text
+              as="h3"
+              fontSize={[2, 3]}
+              color="white"
+              fontWeight="heading"
+              mb={2}
+              sx={{ textTransform: "uppercase" }}
+            >
+              Past
+            </Text>
+          </>
+        )}
+
+        <Box as="ul">
+          {/* display only the first three past events */}
+          {pastEvents.splice(0, 3).map(event => (
+            <MeetupEventCard key={event.id} event={event} />
+          ))}
+        </Box>
 
         <Box textAlign="center" mt={3}>
           <Button
