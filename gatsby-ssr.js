f4b5eee894fe3a398891c63866efc58dfbff7e7e
jromest <jromestiller@gmail.com>
2020-04-24 21:03:17 +0800

    :wrench: disable camelcase and prefer-export-default in eslint

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,6 @@
 import React from "react"
 import RootElement from "./src/root-element"
 
-// eslint-disable-next-line import/prefer-default-export
 export const wrapRootElement = ({ element }) => (
   <RootElement>{element}</RootElement>
 )
