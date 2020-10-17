8c98c242d00ee991ee0a4325b757ceb83b16ef09
galacemiguel <galacemiguel@gmail.com>
2020-10-17 16:34:05 +0800

    Remove unused utility functions

--- src/utils.js
+++ src/utils.js
@@ -1,55 +1,7 @@
-import { useState, useEffect } from "react"
-
 export const generate3dShadow = (size, color) => {
   let result = []
   for (let i = size; i > 0; i -= 1) {
     result = [...result, `${i}px ${i}px ${color}`]
   }
   return result.join()
-}
-
-export const useWindowSize = () => {
-  const isClient = typeof window === "object"
-
-  const getSize = () => ({
-    width: isClient ? window.innerWidth : undefined,
-    height: isClient ? window.innerHeight : undefined,
-  })
-
-  const [windowSize, setWindowSize] = useState(getSize)
-
-  useEffect(() => {
-    if (!isClient) {
-      return false
-    }
-
-    const handleResize = () => {
-      setWindowSize(getSize())
-    }
-
-    window.addEventListener("resize", handleResize)
-
-    return () => window.removeEventListener("resize", handleResize)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  return windowSize
-}
-
-export const useOnClickOutside = (ref, handler) => {
-  useEffect(() => {
-    const listener = e => {
-      if (!ref.current || ref.current.contains(e.target)) {
-        return
-      }
-
-      handler(e)
-    }
-
-    document.addEventListener("click", listener)
-
-    return () => {
-      document.removeEventListener("click", listener)
-    }
-  }, [ref, handler])
 }
