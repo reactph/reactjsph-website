845edafd5031e1c842940d0ca347b0f48aff1c32
galacemiguel <galacemiguel@gmail.com>
2020-10-17 16:33:27 +0800

    Add dev board page link to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,43 +1,13 @@
-import React, { useState, useEffect, useRef } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import { Link as GatsbyLink } from "gatsby"
-import { Box, Flex, Link } from "rebass"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons"
-import { useWindowSize, useOnClickOutside } from "../utils"
-import { Logo } from "../images"
+import { Box, Flex, Link, Text } from "rebass"
+import { Brandmark } from "../images"
 import Container from "./container"
 
-const menus = [
-  { label: "Meet-ups", path: "/meetups" },
-  { label: "Blog", path: "/blog" },
-  { label: "Jobs", path: "/jobs" },
-]
+const menus = [{ label: "Dev Board", path: "/dev-board" }]
 
 const Header = ({ siteTitle }) => {
-  const navRef = useRef()
-
-  const size = useWindowSize()
-  const isMobile = size.width < 640
-
-  const [showMenu, setShowMenu] = useState()
-
-  useOnClickOutside(navRef, () => setShowMenu(false))
-
-  useEffect(() => {
-    if (isMobile) {
-      setShowMenu(false)
-    }
-  }, [isMobile])
-
-  useEffect(() => {
-    if (showMenu) {
-      document.body.style.overflow = "hidden"
-    } else {
-      document.body.style.overflow = "visible"
-    }
-  }, [showMenu])
-
   return (
     <Box
       as="header"
@@ -57,67 +27,44 @@ const Header = ({ siteTitle }) => {
           justifyContent="space-between"
           py={2}
         >
-          <Link as={GatsbyLink} to="/">
-            <Logo title={siteTitle} width="12.5rem" />
-          </Link>
-
-          <Box as="nav" ref={navRef} sx={{ display: "none" }}>
-            <Box
-              as="button"
+          <Flex
+            as={GatsbyLink}
+            to="/"
+            alignItems="center"
+            sx={{ textDecoration: "none" }}
+          >
+            <Box as={Brandmark} title={siteTitle} height="4rem" mr={1} />
+            <Text
+              display={["none", "initial"]}
+              as="h1"
               fontSize={3}
-              display={["block", "none"]}
-              width="45px"
-              sx={{
-                backgroundColor: "transparent",
-                color: "white",
-                border: "2px solid transparent",
-                "&:focus, &:active": {
-                  borderColor: "lightBlue",
-                },
-                position: "relative",
-                zIndex: 1,
-              }}
-              onClick={() => setShowMenu(!showMenu)}
-              aria-hidden={!isMobile}
-              aria-label="Toggle nav"
+              fontWeight="heading"
+              lineHeight={1}
+              sx={{ textTransform: "uppercase" }}
             >
-              <FontAwesomeIcon
-                aria-hidden="true"
-                icon={!showMenu ? faBars : faTimes}
-                size="1x"
-              />
-            </Box>
+              <Text as="span" display="block" color="lightBlue">
+                ReactJS
+              </Text>
+              <Text as="span" display="block" color="gold">
+                Philippines
+              </Text>
+            </Text>
+          </Flex>
 
-            <Flex
-              flexDirection={["column", "row"]}
-              alignItems="center"
-              justifyContent={["flex-start", "center"]}
-              height={["100vh", "100%"]}
-              backgroundColor={["rgba(0, 0, 0, 0.85)", "initial"]}
-              py={[4, 0]}
-              sx={{
-                transition: "right 300ms ease",
-                position: ["absolute", "static"],
-                top: 0,
-                right: showMenu ? 0 : "-250px",
-                bottom: 0,
-                width: ["250px", "100%"],
-                overflow: ["hidden", "visible"],
-              }}
-            >
-              {menus.map(menu => (
+          <ul>
+            {menus.map(menu => (
+              <li>
                 <Link
                   key={menu.label}
                   variant="nav"
                   href={menu.path}
-                  fontSize={[2, "inherit"]}
-                  my={[2, 0]}
+                  fontSize={[1, 2]}
                 >
                   {menu.label}
                 </Link>
-              ))}
-            </Flex>
-          </Box>
+              </li>
+            ))}
+          </ul>
         </Flex>
       </Container>
     </Box>
