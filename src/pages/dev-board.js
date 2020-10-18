f7172d3bcdd5a8a205aed7be04807a534ddc142d
Samuel Suther <info@suther.de>
2020-10-18 14:04:16 +0200

    Update dev-board.js
    Shuffle the order of devs
    This fix #90

--- src/pages/dev-board.js
+++ src/pages/dev-board.js
@@ -8,6 +8,26 @@ import DevBio from "../components/devBio"
 
 import DevsJSON from "../../content/devs.json"
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+const devs = shuffle(DevsJSON);
+
 const SecondPage = () => (
   <Layout>
     <SEO title="Dev Board" />
@@ -64,7 +84,7 @@ const SecondPage = () => (
           rowGap: "2rem",
         }}
       >
-        {DevsJSON.map(dev => (
+        {devs.map(dev => (
           <Box as="li">
             <DevBio dev={dev} />
           </Box>
