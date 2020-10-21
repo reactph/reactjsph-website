fb89dc006a8a93edf01b3ad98002354ddfb3e690
Samuel Suther <info@suther.de>
2020-10-21 17:48:17 +0200

    Update utils.js
    Put shuffleArray to utils.js

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,11 @@ export const generate3dShadow = (size, color) => {
   }
   return result.join()
 }
+
+export const ShuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
